a151890565862c7b85cf0d56a3533196cc77f50c
maromo71m <professormoraes@gmail.com>
2021-01-07 11:26:24 -0300

    Primeiro commit com Material-UI

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>Formulário de Cadastro</h1>
+      <FormularioCadastro/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,32 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+
+function FormularioCadastro(props) {
+    return (
+        <form>
+            <label>Nome</label>
+            <input type="text" />
+
+            <label>Sobrenome</label>
+            <input type="text" />
+
+            <label>CPF</label>
+            <input type="text" />
+
+            <label>Promoções</label>
+            <input type="checkbox" />
+
+            <label>Novidades</label>
+            <input type="checkbox" />
+
+            <Button
+                variant="contained"
+                color="primary"
+                type="submit">
+                Cadastrar
+            </Button>
+        </form>
+    );
+}
+
+export default FormularioCadastro;
